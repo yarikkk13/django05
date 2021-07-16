bf31ce6ef6e7dfb1f2d356285d119aa67155f86d
yarikkk13 <yaroslav.mahas@gmail.com>
2021-07-16 21:20:16 +0300

    основні CRUD

--- apps/car/serializers.py
+++ apps/car/serializers.py
@@ -12,5 +12,9 @@ class CarSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CarModel
-        fields = '__all__'
+        fields = ('id', 'brand', 'model', 'year', 'user')
+        extra_kwargs = {
+            'user': {"write_only": True}
+        }
+        # fields = '__all__'
         # exclude = ('user')
