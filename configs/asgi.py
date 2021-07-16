9397c3dd8d72d27566a2173842e792cf9b04b918
yarikkk13 <yaroslav.mahas@gmail.com>
2021-07-16 16:56:59 +0300

    config.settings, api.urls, managers, customUserManager start models

--- configs/asgi.py
+++ configs/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django05 project.
+ASGI config for configs project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django05.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configs.settings')
 
 application = get_asgi_application()
